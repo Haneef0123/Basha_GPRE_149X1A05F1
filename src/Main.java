5415f9984611c8e87f57cfa9246449b1c24f50a0
Haneef0123 <32005369+Haneef0123@users.noreply.github.com>
2017-09-16 15:38:21 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-		Student st = new Student(01,"Haneef",new Date(1996,05,08),63.568);
+		Student st1 = new Student(01,"Haneef",new Date(1996,05,08),63.568);
 		Student st2 = new Student(02,"Haneef",new Date(1996,05,08),63.568);
 		Student st3 = new Student(03,"Haneef",new Date(1996,05,08),63.568);
 		Student st4 = new Student(04,"Haneef",new Date(1996,05,08),63.568);
